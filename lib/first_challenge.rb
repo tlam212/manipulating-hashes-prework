c4fcc19c2914ad3c3f66e5d2fe1fd80ad748b433
Toni Lamont <tlam212@gmail.com>
2019-04-25 15:41:55 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,6 +17,7 @@ def first_challenge
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
         value.each do |flavor|
+          flavor
           flavor.delete_if("strawberry")
         end
       end
